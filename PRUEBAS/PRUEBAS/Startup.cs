1fa9a8d2071d7a2b1faaa730f4fd93c46238c374
SoulRick <ricart18@gmail.com>
2019-05-29 20:47:45 -0400

    Creación del Proyecto

---
+++ PRUEBAS/PRUEBAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRUEBAS.Startup))]
+namespace PRUEBAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
